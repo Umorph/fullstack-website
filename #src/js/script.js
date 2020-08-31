3b439badbd6cadbea1745d1b365f3313c15b5ef5
Vladislav Kantariya <umorphine@gmail.com>
2020-08-31 16:13:07 +0400

    Site nav refactoring

--- #src/js/script.js
+++ #src/js/script.js
@@ -14,11 +14,24 @@ testWebP(function (support) {
     }
 });
 
-const headerMenuButton = document.querySelector('.header__menu-button'),
-    headerNavigation = document.querySelector('.header__site-navigation'),
-    firstMenuLine = document.querySelector('.button-image__line-first'),
-    secondMenuLine = document.querySelector('.button-image__line-second'),
-    thirdMenuLine = document.querySelector('.button-image__line-third');
+var headerBlock = document.querySelector('.header');
+var headerLink = document.querySelector('.header__link');
+var headerMenu = document.querySelector('.site-navigation');
+var headerButton = document.querySelector('.header__menu-button');
 
-// close nav menu, if js is working:
-headerNavigation.classList.remove('.site-navigation--opened');
+closeHeaderMenu = function () {
+    headerMenu.classList.remove('header__site-navigation--opened');
+    headerButton.classList.remove('header__menu-button--opened');
+    headerBlock.classList.remove('header--blue');
+    headerLink.classList.remove('header__link--white');
+}
+
+// Progressive enhan
+closeHeaderMenu();
+
+headerButton.addEventListener('click', function (evt) {
+    headerMenu.classList.toggle('header__site-navigation--opened');
+    headerButton.classList.toggle('header__menu-button--opened');
+    headerBlock.classList.toggle('header--blue');
+    headerLink.classList.toggle('header__link--white');
+});
